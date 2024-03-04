9b242bd7de0fd83173a30299ee9bf7973da4b66b
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-04 20:43:02 -0300

    criando a tabela de produtos no projeto

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 # passo a passo do projeto
-
 # Passo 1: Entrar na base de dados da empresa
+    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# pip install pyautogui
+
 from time import sleep
 import pyautogui as auto
 
-auto.PAUSE = 1
+auto.PAUSE = 0.5
 
 # Pyautogui.click -> clicar em algum lugar
 # Pyautogui.write -> Escrever textos
@@ -27,7 +29,23 @@ sleep(5)
 # Passo: 2 Fazer Login
 auto.click(x=719, y=442)
 auto.write('pythonimpressionador@gmail.com')
+
+# Escrever a senha
 auto.press('tab')
 auto.write('sua senha aqui')
+
+# Clicar em login
 auto.press('tab')
 auto.press('enter')
+
+# Passo 3: Importar a base de dados
+# pip install pandas numpy openpyxl
+# leitor de pdf tabula
+
+import pandas
+tabela = pandas.read_csv('produtos.csv')
+print(tabela)
+
+
+# Passo 4: Cadastrar 1 Produto
+# Passo 5: Repetir o processo de cadastro até acabar
